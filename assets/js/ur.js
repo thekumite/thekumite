82b7df226987b16b6cab7cf9faf700a0248f47f7
Arne Emil Buchter <5651612@RTS-365.dk>
2019-06-04 09:21:14 +0200

    addur

--- assets/js/ur.js
+++ assets/js/ur.js
@@ -1,7 +1,9 @@
 let countdown;
 const timerDisplay = document.querySelector('.ur__timer');
+const buttons = document.querySelectorAll('.select__time');
 
 function timer(seconds){
+    clearInterval(countdown);
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -11,14 +13,36 @@ function timer(seconds){
 
         if(secondsleft < 0){
             clearInterval(countdown);
-            return;
+            return
         }
         displayTimeLeft(secondsleft);
     }, 1000);
 };
+
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `11:11`;
-    console.log({minutes, remainderSeconds});
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
 }
+
+function startTimer() {
+    let seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+
+buttons.forEach(button => button.addEventListener('click' , function(){
+    let seconds = parseInt(this.dataset.time);
+    timerDisplay.setAttribute('data-time', seconds);
+    displayTimeLeft(seconds);
+}));
+
+let startStop = document.querySelector('.start');
+let resetknap = document.querySelector('.reset');
+
+startStop.addEventListener('click', function(){
+        timer(parseInt(timerDisplay.getAttribute('data-time')));
+        startStop.innerHTML = 'Stop';
+});
