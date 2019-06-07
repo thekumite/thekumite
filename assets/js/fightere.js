b54aec89b538e79da6092e6f90da9ae7fb886801
Arne Emil Buchter <5651612@RTS-365.dk>
2019-06-07 11:28:34 +0200

    addsrcfunktion

---
+++ assets/js/fightere.js
@@ -0,0 +1,27 @@
+
+
+
+<div class="flip-box flip-box--one">
+            <div class="flip-box-inner">
+                <div class="flip-box-front">
+                    <div class="flip-box-front-content-wrapper">
+                        <img class="front-image" src="./assets/img/danielstrækker.PNG" alt="profil-img">
+                        <p>FULL NAME | Daniel Torres</p> 
+                        <p>NATIONALITY | Denmark</p>
+                        <p>HEIGHT | 182 CM</p>
+                        <p>WEIGHT CLASS | 85 KG</p>
+                        <p>AGE | 34</p>
+                    </div>
+                </div>
+                <div class="flip-box-back">
+                    <div class="flip-box-back-content-wrapper">
+                        <img class="back-image" src="./assets/img/wm.jpg" alt="profil-img">
+                        <p class="back-image__text">RECORD | 4-4-0</p> 
+                        <p class="">TITLE | NONE</p>
+                        <p class="">CLASS | C</p>
+                        <p class="">FIGHTING STYLE | MUAY THAI</p>
+                        <P class="">LAST FIGHT | WORLD K-1 CHAMPIONSHIP</P>
+                    </div>
+                </div>
+            </div>
+        </div>
